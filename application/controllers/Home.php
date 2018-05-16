215eee1102d3ca1660da4b05bca6bf6181b32061
gan13 <slidyng6@gmail.com>
2018-05-16 13:51:22 +0700

    fix

--- application/controllers/Home.php
+++ application/controllers/Home.php
@@ -3,7 +3,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 
 class Home extends CI_Controller {
 
-	
+	// eror saya 
 	public function index()
 	{
         $mainView = 'home/index';
