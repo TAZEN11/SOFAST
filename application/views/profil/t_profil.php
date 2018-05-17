1472482c04f0bc40ab52cef95c206e7b976b27f8
gan13 <slidyng6@gmail.com>
2018-05-17 09:55:03 +0700

    00

--- application/views/profil/t_profil.php
+++ application/views/profil/t_profil.php
@@ -141,6 +141,7 @@
   </nav>
 </div>
 <!-- Content Wrapper. Contains page content -->
+<!--  -->
   <div class="content-wrapper">
     <!-- Content Header (Page header) -->
     <section class="content-header">
